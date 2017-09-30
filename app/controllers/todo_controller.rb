dbfef70b72ffcebe5249925821871b6001d02121
Nia Chung <niachung2701@gmail.com>
2017-09-30 17:51:23 +0000

    make the curriculm

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,10 @@
 class TodoController < ApplicationController
     def index
     end
+
+    def show
+        @todo_description = "Make the Curriculum"
+        @todo_promodoro_estimate = 4
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get 'todo/index', to:'todo#index'
 end
+
+Rails.application.routes.draw do
+  get 'todo/show', to:'todo#show'
+end
