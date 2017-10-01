36045c4ea1cc4b19b95eb7d6206d10f30fe9b3c0
Nia Chung <niachung2701@gmail.com>
2017-10-01 16:20:46 +0000

    Add the edit, update, and destroy RCAV flows

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,37 +4,37 @@ class TodoController < ApplicationController
     end
 
     def show
-        todo_id = params[:id]
-        if todo_id == '1'
-            @todo_description = "Make the curriculum"
-            @todo_pomodoro_estimate = 4
-
-        elsif todo_id == '2'
-            @todo_description = "Buy workshop supplies"
-            @todo_pomodoro_estimate = 3
-            
-        elsif todo_id == '3'
-            @todo_description = "Meet with the volunteer trainers"
-            @todo_pomodoro_estimate = 2
-            
-        elsif todo_id == '4'
-            @todo_description = "Order food for saturday and sunday"
-            @todo_pomodoro_estimate = 1
-            
-        elsif todo_id == '5'
-            @todo_description = "Check pre-work assignments"
-            @todo_pomodoro_estimate = 2
-            
-        elsif todo_id == '6'
-            @todo_description = "Send workshop locations to all the students"
-            @todo_pomodoro_estimate = 1
-            
-        elsif todo_id == '7'
-            @todo_description = "Have a great workshop"
-            @todo_pomodoro_estimate = 0
-        end
-        
-        
+        @todo = Todo.find_by_id(params[:id])
     end
-end    
     
+    def new
+    end
+    
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+    
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    
+    def update
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro-estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/todo/index"
+    end
+        
+end    
+
